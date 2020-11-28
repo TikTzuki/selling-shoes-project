674b5139e38055453ba75427a93b7f64d8cbb2d5
vothituyethong <63567624+vothituyethong@users.noreply.github.com>
2020-11-28 17:00:03 +0700

    28/11

--- src/main/java/SellingShoes/service/Product/ProductService.java
+++ src/main/java/SellingShoes/service/Product/ProductService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.lazada.lazop.api.LazopClient;
 import com.lazada.lazop.api.LazopRequest;
 import com.lazada.lazop.api.LazopResponse;
+import com.lazada.lazop.util.FileItem;
 
 @Service
 public class ProductService {
@@ -50,8 +51,22 @@ public class ProductService {
 	}
 
 	//Create product
-	public String createProduct() {
-		
+	public String createProduct(String accessToken, String LazUrl, String appkey, String appSecret,
+			String payload) {
+		LazopClient client = new LazopClient(LazUrl, appkey, appSecret);
+		LazopRequest request = new LazopRequest();
+		request.setApiName("/product/create");
+		//request.setHttpMethod("POST");
+		request.addApiParameter("payload", payload);
+		LazopResponse response = new LazopResponse();
+		try {
+			response = client.execute(request, accessToken);	
+			System.out.println(response.getBody());
+			Thread.sleep(10);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 		return new String("create product success");
 	}
 	
@@ -73,4 +88,99 @@ public class ProductService {
 			</Images>
 		</Sku>
 	 */
+	//Upload Image
+	public String uploadImage(String accessToken, String LazUrl, String appkey, String appSecret,
+			String file) {
+		LazopClient client = new LazopClient(LazUrl, appkey, appSecret);
+		LazopRequest request = new LazopRequest();
+		request.setApiName("/image/upload");
+		FileItem fileImage=new FileItem(file);
+		request.addFileParameter("image",fileImage);
+		LazopResponse response = new LazopResponse();
+		try {
+			response = client.execute(request, accessToken);	
+			System.out.println(response.getBody());
+			Thread.sleep(10);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return new String("upload image success");
+		
+	}
+	//get Brands
+	public String getBrands(String accessToken, String lazUrl, String appkey, String appSecret,			
+			String offset,
+			String limit) {		
+		LazopClient client = new LazopClient(lazUrl, appkey, appSecret);
+		LazopRequest request = new LazopRequest();
+		request.setApiName("/brands/get");
+		request.setHttpMethod("GET");
+		request.addApiParameter("offset", offset);
+		request.addApiParameter("limit", limit);
+		LazopResponse response = new LazopResponse();
+		try {
+			response = client.execute(request, accessToken);
+			Thread.sleep(10);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return response.getBody();
+	}
+	//get Product Item
+	public String getProductItem(String accessToken, String lazUrl, String appkey, String appSecret,			
+			String item_id,
+			String seller_sku) {		
+		LazopClient client = new LazopClient(lazUrl, appkey, appSecret);
+		LazopRequest request = new LazopRequest();
+		request.setApiName("/product/item/get");
+		request.setHttpMethod("GET");
+		request.addApiParameter("item_id", item_id);
+		request.addApiParameter("seller_sku", seller_sku);
+		LazopResponse response = new LazopResponse();
+		try {
+			response = client.execute(request, accessToken);
+			Thread.sleep(10);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return response.getBody();
+	}
+	//remove Product
+	public String removeProduct(String accessToken, String lazUrl, String appkey, String appSecret,			
+			String seller_sku_list) {		
+		LazopClient client = new LazopClient(lazUrl, appkey, appSecret);
+		LazopRequest request = new LazopRequest();
+		request.setApiName("/product/remove");
+		request.addApiParameter("seller_sku_list", seller_sku_list);
+		LazopResponse response = new LazopResponse();
+		try {
+			response = client.execute(request, accessToken);
+			Thread.sleep(10);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return response.getBody();
+	}
+	//update Product
+	public String updateProduct(String accessToken, String lazUrl, String appkey, String appSecret,			
+			String payload) {		
+		LazopClient client = new LazopClient(lazUrl, appkey, appSecret);
+		LazopRequest request = new LazopRequest();
+		request.setApiName("/product/update");
+		request.addApiParameter("payload", payload);
+		LazopResponse response = new LazopResponse();
+		try {
+			response = client.execute(request, accessToken);
+			Thread.sleep(10);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return response.getBody();
+	}
+
 }
